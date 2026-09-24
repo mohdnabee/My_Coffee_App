6a8cf28d8db30f5dd0832b2cb114551e9b2fd048
Mohd Nabeel <163665782+mohdnabee@users.noreply.github.com>
2026-09-24 23:42:02 +0530

    Categoreis chip

---
+++ app/src/main/java/com/nabeel/mycoffeeapp/Screens/HomeScreen/HomeScreenCategories.kt
@@ -0,0 +1,40 @@
+package com.nabeel.mycoffeeapp.Screens.HomeScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true  )
+@Composable
+fun HomeScreenCategories() {
+
+    val  categories =  listOf("All Coffees" , "Macchiato" ,  "Latte", "Americano", "Cappuccino" , "snacks" , "Desserts")
+
+    var selectedCategory  by remember {mutableStateOf(categories.first()) }
+
+    LazyRow(
+        modifier = Modifier.padding(horizontal = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        items(categories) {category ->
+            CategoryChip(
+                text =  category ,
+                isSelected = category ==  selectedCategory,
+                onSelected = {selectedCategory =  category}
+            )
+        }
+
+    }
+
+
+}
