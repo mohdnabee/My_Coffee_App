0439fdb55329de4ef4d53e87d305c122c21101c6
Mohd Nabeel <163665782+mohdnabee@users.noreply.github.com>
2026-09-25 10:14:44 +0530

    data product

---
+++ app/src/main/java/com/nabeel/mycoffeeapp/model/Product.kt
@@ -0,0 +1,9 @@
+package com.nabeel.mycoffeeapp.model
+
+data class Product (
+    val id : Int ,
+    val  name : String,
+    val description : String,
+    val  price : Double,
+    val imageRes : Int
+)
